ef63e25c5210b5a1957da234e977c6342b056720
Mon-san-Michel <79431569+Mon-san-Michel@users.noreply.github.com>
2025-02-18 22:22:30 +0900

    20250218-10

--- simpledice.js
+++ simpledice.js
@@ -4,7 +4,7 @@ const dice_num = document.getElementById("dice_num");
 const dice_size = document.getElementById("dice_size");
 
 //出力先（過去の結果）と現在時刻
-const output = document.getElementById("output");
+const output = document.getElementById("output_list");
 const timelog = new Date().toLocaleString();
 
 //実行ボタンが押されたときの処理
@@ -18,5 +18,5 @@ roll_btn.addEventListener('click', () => {
 		roll_total += roll;
     }
 	//今回の結果と時刻を出力に追加
-	output.innerHTML += `<div id = "output"><span id = "output_dice">${dice_num.value}D${dice_size.value}＞${roll_total}</span><div id ="output_num">{${roll_output}}</div><span id="output_time">${timelog}</span></div><hr>`;
+	output.innerHTML += `<li id = "output"><span id = "output_dice">${dice_num.value}D${dice_size.value}＞${roll_total}</span><div id ="output_num">{${roll_output}}</div><span id="output_time">${timelog}</span></li><hr>`;
 });
